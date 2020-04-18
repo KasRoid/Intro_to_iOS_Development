81b8a9edf7d0ccf0cfadec9e586e0dc4b0bcec98
KasRoid <KasRoid@gmail.com>
2020-04-18 15:46:24 +0900

    Storyboard tutorial

---
+++ Code_with_Chris/AutoLayout_Practice/AutoLayout_Practice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayout_Practice
+//
+//  Created by Kas Song on 2020.04.17.
+//  Copyright © 2020 Kas Song. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
